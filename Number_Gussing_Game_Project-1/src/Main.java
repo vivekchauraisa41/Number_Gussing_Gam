c7d5d89e6a45627888e8fa5bbc2148077c203023
vivekchauraisa41 <145891081+vivekchauraisa41@users.noreply.github.com>
2023-09-24 11:48:26 +0530

    Add files via upload

---
+++ Number_Gussing_Game_Project-1/src/Main.java
@@ -0,0 +1,119 @@
+import java.util.*;
+public class Main {
+    static ArrayList<Integer> scoreBoard = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        Main.menu(scoreBoard);
+    }
+    public static void menu(ArrayList<Integer> scoreBoard) {
+        Main methodChange = new Main();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("1) Play Game");
+        System.out.println("2) Score Board");
+        System.out.println("3) Exit");
+
+        try{
+        System.out.print("Select one above");
+        int menuOption = input.nextInt();
+
+        switch (menuOption) {
+            case 1:
+                System.out.print("\n" + "What would you like the range of game? ");
+                int numberRange = input.nextInt();
+
+                int randomNumber = methodChange.randomNumber(numberRange);
+                methodChange.guessNumber(randomNumber);
+                break;
+            case 2:
+                methodChange.displayScoreBoard();
+                break;
+            case 3:
+                System.out.println("\n" + "Thanks for playing!");
+                System.exit(1);
+                break;
+            default:
+                throw new InputMismatchException("Invalid entry, Please Try again");
+        }
+    }
+    catch(InputMismatchException e){
+            System.err.println("\n" + e.getMessage() + "\n");
+        menu(scoreBoard);
+    }
+        }
+        public int randomNumber(int numberRange) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(numberRange) + 1;
+
+        return randomNumber;
+    }
+    public void guessNumber(int randomNumber) {
+        Scanner input = new Scanner(System.in);
+
+        int userGuess;
+        int guess = 0;
+
+        do {
+            System.out.print("Enter your guess: ");
+            userGuess = input.nextInt();
+            guess++;
+
+            if (userGuess > randomNumber) {
+                System.out.println("Lower");
+            } else if (userGuess < randomNumber) {
+                System.out.println("Higher");
+            }
+
+        }
+        while (randomNumber != userGuess);
+
+        System.out.println(" ");
+
+        if (guess == 1) {
+            System.out.println("You answered right in " + guess + " try");
+        }
+        else {
+            System.out.println("You answered right in " + guess + " tries");
+        }
+
+        if(guess < 5) {
+            System.out.println("Score is 1000");
+        } else if (guess < 10) {
+            System.out.println("Score is 800");
+        }
+        else if (guess < 15) {
+            System.out.println("Score is 600");
+        }
+        else if (guess < 20) {
+            System.out.println("Score is 400");
+        }
+        else if (guess < 25) {
+            System.out.println("Score is 200");
+        }
+
+        scoreBoard.add(guess);
+        System.out.println(" ");
+
+        menu(scoreBoard);
+    }
+
+    public void displayScoreBoard() {
+        int guess = 0;
+        int score = 0;
+        System.out.println("********************************");
+        System.out.println("Score Board");
+        System.out.println("********************************");
+
+        System.out.println("Your fastest games today: " + "\n");
+        Collections.sort(scoreBoard);
+
+        for (Integer scores : scoreBoard) {
+            System.out.println("Finished the game in " + scores + " tries");
+        }
+
+        System.out.println(" ");
+        menu(scoreBoard);
+
+
+    }
+}
